15049d27617b9731871e29116157f97f28b0c916
IKei105 <natsuki.kei105@gmail.com>
2025-06-29 13:52:15 +0900

    Refactor ProfilesController and update views

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,24 @@
 class ProfilesController < ApplicationController
+
   def new
+    @profile = Profile.new
   end
 
   def create
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      flash.now[:alert] = "正しく入力してくださいよ"
+      logger.debug @profile.errors.full_messages.join(", ")
+      render :new
+    end
   end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:introduction, :blog_url)
+  end
+
 end
