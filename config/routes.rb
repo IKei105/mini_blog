7230f2b03cd1192791cd07011a0321823e5a8e73
IKei105 <natsuki.kei105@gmail.com>
2025-06-30 17:04:38 +0900

    Remove profile model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :posts, only: [:index, :create] 
   root to: 'posts#index'
-
-  resources :profiles, only: [:new, :create]
 end
