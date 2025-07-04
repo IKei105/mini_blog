0e7640bb865b11675758620470a1fc868da19745
IKei105 <natsuki.kei105@gmail.com>
2025-07-05 08:09:53 +0900

    feat: add follow/unfollow logic

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,8 +2,12 @@ class FollowsController < ApplicationController
   def create
     follow_user = User.find(params[:follow_user_id])
     current_user.follows.create(follow_user: follow_user)
+    redirect_to root_path 
   end
 
   def destroy
+    follow_user = current_user.follows.find_by(follow_user_id: params[:id])
+    follow_user.destroy if follow_user
+    redirect_to root_path 
   end
 end
