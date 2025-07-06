871a756f7943199b0d62806aa7239c4cbcc9f1b4
IKei105 <natsuki.kei105@gmail.com>
2025-07-07 03:26:25 +0900

    Add system specs for user follow/unfollow and following timeline

--- spec/system/follow/follow_spec.rb
+++ spec/system/follow/follow_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "User follow/unfollow", type: :system do
-  let!(:user) { create(:user) }
-  let!(:other_user) { create(:user) }
+  let!(:user) { create(:user, password: "password123", password_confirmation: "password123") }
+  let!(:followed_user) { create(:user) }
 
   before do
     driven_by(:rack_test)
 
-    # ログイン処理
+    create(:post, user: followed_user, content: "こんにちは")
+
     visit new_user_session_path
     fill_in "ユーザーID", with: user.userid
     fill_in "パスワード", with: "password123"
     click_button "ログイン"
 
-    # other_user の投稿を作成して root_path で表示されるようにする
-    create(:post, user: other_user, content: "こんにちは")
-
     visit root_path
   end
 
-  it "他ユーザーをフォローでき、フォロー解除もできる" do
+  it "フォロー・フォロー解除ボタンが切り替わる" do
     expect(page).to have_button("フォロー")
 
     click_button "フォロー"
@@ -27,5 +25,19 @@ RSpec.describe "User follow/unfollow", type: :system do
 
     click_button "フォロー中"
     expect(page).to have_button("フォロー")
+  end
+
+  it "フォロー中タイムラインにフォローしたユーザーの投稿のみ表示される" do
+    click_button "フォロー"
+    expect(page).to have_button("フォロー中")
+
+    click_link "フォロー中"
+
+    expect(page).to have_content("こんにちは")
+
+    unfollowed_user = create(:user)
+    create(:post, user: unfollowed_user, content: "未フォロー投稿")
+    visit posts_following_path
+    expect(page).not_to have_content("未フォロー投稿")
   end
 end
