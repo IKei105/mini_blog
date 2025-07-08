5f3f68d924e0d8f77ae51eda89039df3f805ffcc
IKei105 <natsuki.kei105@gmail.com>
2025-07-08 17:39:29 +0900

    gemfileとrouteのコメントの削除と改行を削除、並び替えをしました

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get "follows/create"
-  get "follows/destroy"
   devise_for :users, controllers: { registrations: "users/registrations" }
-  resources :posts, only: [ :index, :create ]
-  get "posts/following", to: "posts#following"
-  root to: "posts#index"
 
-  resources :users, only: [ :show ]
-  resources :follows, only: [ :create, :destroy ]
+  root to: "posts#index"
 
   namespace :api do
     resources :follows, only: [:create, :destroy]
   end
+
+  resources :posts, only: [ :index, :create ]
+  resources :users, only: [ :show ]
 end
